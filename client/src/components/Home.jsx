d402e4923c7f1b51011103e89b8553997952f57a
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-03-09 11:44:47 +0300

    some change

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100 items-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to Our App! 🎉
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
