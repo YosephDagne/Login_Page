e945f52243a56cba111b160c013b3ab29ace244d
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-03-09 11:47:53 +0300

    styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100 items-center">
+    <div className="flex items-center justify-center h-screen w-full bg-gray-100 text-center">
       <h1 className="text-4xl font-bold text-blue-600">
         Welcome to Our App! 🎉
       </h1>
